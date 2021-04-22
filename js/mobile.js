fa289801ea02d5c45218a70307d3f52596c98523
SPDcarpentrycontractorsSite <82976576+SPDcarpentrycontractorsSite@users.noreply.github.com>
2021-04-22 12:46:57 +0100

    JS menu

--- js/mobile.js
+++ js/mobile.js
@@ -30,7 +30,7 @@ window.onload = function(){
 		if(b){
 			sub.removeAttribute('style');
 			getElm[i].lastChild.style.backgroundImage='url(images/mobile-expand.png)';
-			getElm[i].lastChild.style.backgroundColor='rgba(11, 163, 156, 0.7)';
+			getElm[i].lastChild.style.backgroundColor='rgba(255, 221, 175, 0.9)';
 		} else {
 			sub.style.display='block';
 			getElm[i].lastChild.style.backgroundImage='url(images/mobile-collapse.png)';
